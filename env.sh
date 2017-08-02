cb086560042e3785bec05b4b62d7642e4d95a7b7
Teresa Liu <teresali@usc.edu>
2017-08-02 16:50:38 -0700

    Delete env.sh

--- env.sh
+++
@@ -1,3 +0,0 @@
-export MONGODB_URI="mongodb://admin:admin@ds157682.mlab.com:57682/doorstep"
-export GOOGLE_CLIENT_ID="287065366259-1n87qi1ruqq8vv3i33arlcvfljkeqt7p.apps.googleusercontent.com"
-export GOOGLE_CLIENT_SECRET="BWde0UEWbc4A5xNOEojwUqPb"
