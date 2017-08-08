74edc20ce485dd7595aac63d380aa048646a269d
teresaliu20 <teresali@usc.edu>
2017-08-08 16:42:30 -0700

    Added modal

--- frontend/pages/CommunitiesSearch.js
+++ frontend/pages/CommunitiesSearch.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { getAllCommunities } from '../actions/getAllCommunities';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import CreateCommunityModal from './CreateCommunityModal';
 
 class CommunitySearch extends React.Component {
   constructor(props) {
@@ -26,6 +27,7 @@ class CommunitySearch extends React.Component {
         <Navbar />
         <div className="search-splash">
           <div className="create-community-button">Create a community</div>
+          <CreateCommunityModal />
           <h1 className="title">Join a community</h1>
         </div>
         <SearchCommunitiesList communities={this.props.allCommunities.data}/>
