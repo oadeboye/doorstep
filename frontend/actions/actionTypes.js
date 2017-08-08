5bdc1eb839fa248fa07c2c147ef499f9bf254b40
Minh To <to.nguyet.minh@gmail.com>
2017-08-08 11:25:33 -0700

    fixing memberslist

--- frontend/actions/actionTypes.js
+++ frontend/actions/actionTypes.js
@@ -4,5 +4,8 @@ export default {
   getAllCommunitiesRejected: 'GET_ALL_COMMUNITIES_REJECTED',
   getOneCommunityRequested: 'GET_ONE_COMMUNITY_REQUESTED',
   getOneCommunityFulfilled: 'GET_ONE_COMMUNITY_FULFILLED',
-  getOneCommunityRejected: 'GET_ONE_COMMUNITY_REJECTED'
+  getOneCommunityRejected: 'GET_ONE_COMMUNITY_REJECTED',
+  getAllUsersRequested: 'GET_ALL_USERS_REQUESTED',
+  getAllUsersFulfilled: 'GET_ALL_USERS_FULFILLED',
+  getAllUsersRejected: 'GET_ALL_USERS_REJECTED'
 };
--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import allCommunitiesReducer from './allCommunitiesReducer';
 import oneCommunityReducer from './oneCommunityReducer';
+import allUsersReducer from './allUsersReducer';
 
 const appReducer = combineReducers({
   user: userReducer,
   allCommunities: allCommunitiesReducer,
-  currentComm: oneCommunityReducer
+  currentComm: oneCommunityReducer,
+  allUsers: allUsersReducer,
 });
 
 export default appReducer;
