29a2c4cba0ebe12189dfeb47dbd04619fa5cc1d3
Minh To <to.nguyet.minh@gmail.com>
2017-08-16 14:14:47 -0700

    request styling

---
+++ frontend/components/ImageUpload.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+      imagePreviewUrl: ''};
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  render() {
+    const {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput"
+            type="file"
+            onChange={(e)=>this._handleImageChange(e)} />
+          {/* <button className="submitButton"
+            type="submit"
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button> */}
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageUpload;
